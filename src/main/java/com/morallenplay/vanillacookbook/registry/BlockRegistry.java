5a8d8fd8b4258fa99f34a03d97cb74c81170fb3f
Benjamin LE SIRE <benjaminlesire@yahoo.com>
2023-12-29 16:55:21 +0100

    Fix block properties

--- src/main/java/com/morallenplay/vanillacookbook/registry/BlockRegistry.java
+++ src/main/java/com/morallenplay/vanillacookbook/registry/BlockRegistry.java
@@ -11,7 +11,7 @@ import com.morallenplay.vanillacookbook.block.SliceCakeBlock;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.PushReaction;
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -24,18 +24,18 @@ public class BlockRegistry {
 			VanillaCookbook.MOD_ID);
 	
 	
-	public static final RegistryObject<Block> BERRY_CAKE = BLOCKS.register("berry_cake", () -> new CakesBlock(BlockBehaviour.Properties.of(Material.CAKE).strength(0.5F).sound(SoundType.WOOL)));
-	public static final RegistryObject<Block> CARROT_CAKE = BLOCKS.register("carrot_cake", () -> new CakesBlock(BlockBehaviour.Properties.of(Material.CAKE).strength(0.5F).sound(SoundType.WOOL)));
-	public static final RegistryObject<Block> CHEESECAKE = BLOCKS.register("cheesecake", () -> new CheesecakeBlock(BlockBehaviour.Properties.of(Material.CAKE).strength(0.5F).sound(SoundType.WOOL)));
-	public static final RegistryObject<Block> CHOCOLATE_CAKE = BLOCKS.register("chocolate_cake", () -> new CakesBlock(BlockBehaviour.Properties.of(Material.CAKE).strength(0.5F).sound(SoundType.WOOL)));
-	public static final RegistryObject<Block> MOHNKUCHEN = BLOCKS.register("mohnkuchen", () -> new SliceCakeBlock(BlockBehaviour.Properties.of(Material.CAKE).strength(0.5F).sound(SoundType.WOOL)));
-	public static final RegistryObject<Block> ICE_CREAM_CAKE = BLOCKS.register("ice_cream_cake", () -> new CakesBlock(BlockBehaviour.Properties.of(Material.CAKE).strength(0.5F).sound(SoundType.WOOL)));
-	public static final RegistryObject<Block> ROSE_CAKE = BLOCKS.register("rose_cake", () -> new CakesBlock(BlockBehaviour.Properties.of(Material.CAKE).strength(0.5F).sound(SoundType.WOOL)));
-	public static final RegistryObject<Block> BOOK_CAKE = BLOCKS.register("book_cake", () -> new BookCakeBlock(BlockBehaviour.Properties.of(Material.CAKE).strength(0.5F).sound(SoundType.WOOL)));
-	public static final RegistryObject<Block> PANCAKE_STACK = BLOCKS.register("pancake_stack", () -> new PancakeStackBlock(BlockBehaviour.Properties.of(Material.CAKE).strength(0.5F).sound(SoundType.WOOL)));
-	public static final RegistryObject<Block> CHORUS_CAKE = BLOCKS.register("chorus_cake", () -> new ChorusCakeBlock(BlockBehaviour.Properties.of(Material.CAKE).strength(0.5F).sound(SoundType.WOOL)));
-	public static final RegistryObject<Block> GLOW_BERRY_CAKE = BLOCKS.register("glow_berry_cake", () -> new CakesBlock(Block.Properties.of(Material.CAKE).strength(0.5F).sound(SoundType.WOOL).lightLevel((p_152684_) -> {
+	public static final RegistryObject<Block> BERRY_CAKE = BLOCKS.register("berry_cake", () -> new CakesBlock(BlockBehaviour.Properties.of().pushReaction(PushReaction.DESTROY).strength(0.5F).sound(SoundType.WOOL)));
+	public static final RegistryObject<Block> CARROT_CAKE = BLOCKS.register("carrot_cake", () -> new CakesBlock(BlockBehaviour.Properties.of().pushReaction(PushReaction.DESTROY).strength(0.5F).sound(SoundType.WOOL)));
+	public static final RegistryObject<Block> CHEESECAKE = BLOCKS.register("cheesecake", () -> new CheesecakeBlock(BlockBehaviour.Properties.of().pushReaction(PushReaction.DESTROY).strength(0.5F).sound(SoundType.WOOL)));
+	public static final RegistryObject<Block> CHOCOLATE_CAKE = BLOCKS.register("chocolate_cake", () -> new CakesBlock(BlockBehaviour.Properties.of().pushReaction(PushReaction.DESTROY).strength(0.5F).sound(SoundType.WOOL)));
+	public static final RegistryObject<Block> MOHNKUCHEN = BLOCKS.register("mohnkuchen", () -> new SliceCakeBlock(BlockBehaviour.Properties.of().pushReaction(PushReaction.DESTROY).strength(0.5F).sound(SoundType.WOOL)));
+	public static final RegistryObject<Block> ICE_CREAM_CAKE = BLOCKS.register("ice_cream_cake", () -> new CakesBlock(BlockBehaviour.Properties.of().pushReaction(PushReaction.DESTROY).strength(0.5F).sound(SoundType.WOOL)));
+	public static final RegistryObject<Block> ROSE_CAKE = BLOCKS.register("rose_cake", () -> new CakesBlock(BlockBehaviour.Properties.of().pushReaction(PushReaction.DESTROY).strength(0.5F).sound(SoundType.WOOL)));
+	public static final RegistryObject<Block> BOOK_CAKE = BLOCKS.register("book_cake", () -> new BookCakeBlock(BlockBehaviour.Properties.of().pushReaction(PushReaction.DESTROY).strength(0.5F).sound(SoundType.WOOL)));
+	public static final RegistryObject<Block> PANCAKE_STACK = BLOCKS.register("pancake_stack", () -> new PancakeStackBlock(BlockBehaviour.Properties.of().pushReaction(PushReaction.DESTROY).strength(0.5F).sound(SoundType.WOOL)));
+	public static final RegistryObject<Block> CHORUS_CAKE = BLOCKS.register("chorus_cake", () -> new ChorusCakeBlock(BlockBehaviour.Properties.of().pushReaction(PushReaction.DESTROY).strength(0.5F).sound(SoundType.WOOL)));
+	public static final RegistryObject<Block> GLOW_BERRY_CAKE = BLOCKS.register("glow_berry_cake", () -> new CakesBlock(Block.Properties.of().pushReaction(PushReaction.DESTROY).strength(0.5F).sound(SoundType.WOOL).lightLevel((p_152684_) -> {
 	      return 14 - p_152684_.getValue(CakesBlock.BITES);
 	   })));
-	public static final RegistryObject<Block> BROWNIE_TRAY = BLOCKS.register("brownie_tray", () -> new BrownieTrayBlock(BlockBehaviour.Properties.of(Material.CAKE).strength(0.5F).sound(SoundType.WOOL)));
+	public static final RegistryObject<Block> BROWNIE_TRAY = BLOCKS.register("brownie_tray", () -> new BrownieTrayBlock(BlockBehaviour.Properties.of().pushReaction(PushReaction.DESTROY).strength(0.5F).sound(SoundType.WOOL)));
 }
